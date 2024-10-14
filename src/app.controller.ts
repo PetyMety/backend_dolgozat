fab1bcdfd73bfd37a44e2490b3cf003ada611487
Petrik Diak <diak@petrik.hu>
2024-10-14 12:26:12 +0200

    1mentés

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  /*
   @Get()
   @Render('index')
   getHello() {
@@ -12,4 +12,23 @@ export class AppController {
       message: this.appService.getHello()
     };
   }
+  */
+
+@Get('mozi')
+@Render('mozi')
+submitMozi1() {return {error: []}}
+
+@Post('mozi')
+submitMozi2(@Body(ValidationPipe) moziData: any){
+
+  const {name, email, datum, viewers} = moziData;
+
+  if (!name || !email || !datum || !viewers) {
+    return {error: 'Kérem töltse ki az összes mezőt!'}
+  }
+
+  const emailRegex = /^(?:\d{1}@\d{1})$/;
+
+}
+
 }
